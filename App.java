41ec9c413b6dd71a00918acd53031606e8c5b567
Dhan Micheal Tamparong <111488321+su1kagg@users.noreply.github.com>
2022-09-08 14:50:22 +0800

    Create App.java

---
+++ App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+
+        Student dhan = new Student();
+
+        dhan.firstname = "Dhan";
+        dhan.surname = "Tamparong";
+        dhan.dateOFBirth = "September 30";
+        dhan.studentNumber = 2022101424;
+        dhan.EmailAddress = "tamparongdl@students.national-u.edu.ph";
+        dhan.iAmAwesome = true;
+        dhan.myFullName();
+        dhan.myStudentNumber();
+        dhan.sayMyEmail();
+        dhan.amIAwesome();
+
+    }
+}
